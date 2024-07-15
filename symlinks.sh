2f686bee714ee421fddd2337524008d772ee59f9
gerben <gerben.burgers@gmail.com>
2024-07-15 14:59:07 +0200

    change dotfiles link scripts

--- symlinks.sh
+++
@@ -1,9 +0,0 @@
-# Dotfiles Fedora symlinks
-
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.tmux ~/.tmux
-ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
-ln -s ~/dotfiles/.config/nvim ~/.config/nvim
